1ef70c3f3d4fb0a8583cedc7ee8755f032fec57d
happysysy <happy-sysy@hanmail.net>
2020-07-29 15:10:39 +0900

    메일 모듈 완료

--- src/main/java/dea/homepage/config/WebConfig.java
+++ src/main/java/dea/homepage/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Import({ JpaDatabaseConfig.class })
+//@Import({ JpaDatabaseConfig.class })
 public class WebConfig {
     @Configuration
     @EnableWebMvc
